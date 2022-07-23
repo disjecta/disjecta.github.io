ad46ff4047baf5230fc210d2927c097a8daeb48e
Venteto <g0jtb5nre@relay.firefox.com>
2022-07-23 18:04:02 -0400

    update build.sh + now have GH and GL use it instead of py script

--- build.sh
+++ build.sh
@@ -4,52 +4,59 @@ function __sepr () {
     printf '# ----------------------------------------------------------\n'
 }
 
-__sepr
-cat /etc/os-release
+function __diagnostics () {
+
+    __sepr
+    cat /etc/os-release
+
+    __sepr
+    bash --version
+
+    __sepr
+    which python
+    which python3
+
+    __sepr
+    which pip
+    which pip3
+
+    __sepr
+    which pipenv
+
+    __sepr
+    python -V
+
+    __sepr
+    python3 -V
+
+    __sepr
+    pip -V
+
+    __sepr
+    pip install --upgrade pip
+
+    __sepr
+    pip list
+}
+
+__diagnostics
 
 __sepr
-bash --version
+pip install -r aaa/deps/pip/prod.pip
 
-__sepr
-which python
-which python3
-
-__sepr
-which pip
-which pip3
-
-__sepr
-which pipenv
-
-__sepr
-python -V
-
-__sepr
-python3 -V
-
-__sepr
-pip -V
+# __sepr
+# python manage.py check
 
 __sepr
 mkdir -pv public_collect_static
 mkdir -pv public
 
 __sepr
-pip install --upgrade pip
-
-__sepr
-pip list
-
-__sepr
-pip install -r aaa/deps/pip/prod.pip
-
-__sepr
-# python manage.py check
-
 python3 manage.py collectstatic --noinput \
     --settings aaa.settings.prod \
     --ignore admin \
     --ignore django_non_dark_admin
 
+__sepr
 python3 manage.py distill-local --force \
     --settings aaa.settings.prod
